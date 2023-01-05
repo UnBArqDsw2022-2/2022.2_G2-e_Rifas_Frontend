b09d38b2d409935c0f58292e282811ddb4829fac
ArthurMeloG <arthurdemelo2001@gmail.com>
2023-01-05 18:04:10 -0300

    wip Co-authored-by: eliasyousef00 <eliasyousef00@gmail.com>

--- my-app/src/components/form/index.tsx
+++ my-app/src/components/form/index.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Navbar from "../navbar";
 
 const Form = () => {
   return (
     <div>
-      <h1>Form</h1>
+      <Navbar />
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Box bgcolor={"rgba(23, 114, 160, 0.4)"}>
+          <TextField
+            id="outlined-basic"
+            label="Entre com seu e-mail"
+            variant="outlined"
+            style={{
+              backgroundColor: "var(--c1-color)",
+            }}
+          />
+        </Box>
+
+        <TextField
+          id="outlined-basic"
+          type="password"
+          autoComplete="current-password"
+          label="Standard"
+          variant="outlined"
+        />
+      </Box>
     </div>
   );
 };
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Form from "../components/form";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Form />
     </div>
   );
 };
