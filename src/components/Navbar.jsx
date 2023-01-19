d3ac410b61dda3dc4ee26f645cc92aef37b0eb84
Andre Alves <andremralves@gmail.com>
2023-01-19 14:55:15 -0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,30 +5,32 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import { Link as RouterLink } from 'react-router-dom'
-import { Link } from '@mui/material'
+import { Container, Link } from '@mui/material'
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }} marginBottom={2}>
       <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link
-              underline="none"
-              color="inherit"
-              component={RouterLink}
-              to="/"
-            >
-              Logo
-            </Link>
-          </Typography>
-          <Button color="inherit" component={RouterLink} to="/login">
-            Login
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/register">
-            Comece já
-          </Button>
-        </Toolbar>
+        <Container>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link
+                underline="none"
+                color="inherit"
+                component={RouterLink}
+                to="/"
+              >
+                Logo
+              </Link>
+            </Typography>
+            <Button color="inherit" component={RouterLink} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={RouterLink} to="/register">
+              Comece já
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   )
