f3f0e7f5853b2b34bd6831a68985391529875eff
Andre Alves <andremralves@gmail.com>
2023-01-27 09:23:39 -0300

    update protectedRoute's children element

--- my-app/src/helpers/privateRoute.tsx
+++ my-app/src/helpers/privateRoute.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react'
 import { useKeycloak } from '@react-keycloak/web'
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 
-const PrivateRoute = (children: ReactNode) => {
+const PrivateRoute = ({ children }: { children: ReactJSXElement }) => {
   const { keycloak } = useKeycloak()
 
   const isLoggedIn = keycloak.authenticated
