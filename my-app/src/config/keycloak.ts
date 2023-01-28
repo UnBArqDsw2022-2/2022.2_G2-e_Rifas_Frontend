d1dcba3fe0a8199749ceacfd6e2848e534c30ec9
Andre Alves <andremralves@gmail.com>
2023-01-28 14:11:18 -0300

    add keycloak config file

--- my-app/src/config/keycloak.ts
+++ my-app/src/config/keycloak.ts
@@ -1,8 +1,8 @@
 import Keycloak from 'keycloak-js'
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/auth',
-  realm: 'Keycloak_react',
-  clientId: 'React.auth',
+  url: 'http://localhost:8082/auth',
+  realm: 'master',
+  clientId: 'admin-cli',
 })
 
 export default keycloak
