35f886c7361cafd8f6fb3a9221c0c114bd5a8abf
Andre Alves <andremralves@gmail.com>
2023-01-30 19:06:43 -0300

    add rifa validation apis

--- my-app/src/api/services/rifa.service.ts
+++ my-app/src/api/services/rifa.service.ts
@@ -1,17 +1,59 @@
-import axios from "../config/plugin/axios";
-import Rifa from "../models/rifa.model";
+import axios from '../config/plugin/axios'
+import Rifa from '../models/rifa.model'
 
 class RifaService {
-
-    async criarRifa(rifa: Rifa) {
-        try {
-            const response = await axios.post('/rifa', rifa);
-            console.log(response);
-        } catch (error) {
-            console.error(error);
-        }
+  async criarRifa(rifa: Rifa) {
+    try {
+      const response = await axios.post('/rifa', rifa)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
     }
+  }
 
+  async getRifa(id: number) {
+    try {
+      const response = await axios.get<Rifa>(`/rifa/${id}`)
+      console.log(response)
+      return response
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async aprovarRifa(id: number) {
+    try {
+      const response = await axios.put(`/rifa/aprovar/${id}`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async cancelarRifa(id: number) {
+    try {
+      const response = await axios.put(`/rifa/cancelar/${id}`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async analisarRifa(id: number) {
+    try {
+      const response = await axios.put(`/rifa/analise/${id}`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async fecharRifa(id: number) {
+    try {
+      const response = await axios.put(`/rifa/fechar/${id}`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 
 export default RifaService
