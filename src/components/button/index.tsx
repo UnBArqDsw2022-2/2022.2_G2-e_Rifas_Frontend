8cfcc7d29de8b137367955da82a2e5acfe05dae6
ErickMVdO <erickmelovidal@gmail.com>
2023-01-30 22:54:07 -0300

    corrigindo tela home

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 type props = {
-  onClickButton: () => void;
+  onClickButton?: () => void;
   textButton: string;
   style?: {}
 };
